b0885b35c97ecf3c472a84566f905ad5a29da49d
Swark Yang <syang@axiado.com>
2025-10-01 04:21:24 -0700

    soc: axiado: Add initial support and SGPIO driver
    This patch introduces the initial platform support for Axiado SoCs.
    It adds the necessary directory structure, Kconfig options, and Makefile
    hooks under drivers/soc/axiado/.
    
    As the first driver for this new platform, this commit adds the driver
    for the Axiado SGPIO controller.
    
    The Kconfig options SOC_AXIADO and AXIADO_SGPIO are introduced to
    allow compile-time configuration of this support
    
    Signed-off-by: Tzu-Hao Wei <twei@axiado.com>
    Signed-off-by: Swark Yang <syang@axiado.com>

---
+++ drivers/soc/axiado/sgpio-axiado.c
@@ -0,0 +1,520 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Axiado Serial GPIO (SGPIO) Driver
+ *
+ * This driver provides support for Axiado's Serial GPIO controller, which
+ * implements a serial interface for GPIO operations. The driver supports both
+ * 128-bit and 512-bit design variants and provides GPIO functionality.
+ *
+ * Copyright (c) 2021-25 Axiado Corporation.
+ */
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
+#include <linux/cdev.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
+#include <linux/io.h>
+#include <linux/clk.h>
+#include <linux/spinlock.h>
+#include <linux/string.h>
+#include <linux/kernel.h>
+#include <linux/irq.h>
+#include <linux/interrupt.h>
+#include <linux/gpio/driver.h>
+#include <linux/types.h>
+
+#include <linux/spi/spi.h>
+#include <linux/of_irq.h>
+#include <linux/delay.h>
+#include <linux/gpio.h>
+
+#include <linux/ktime.h>
+#include <linux/printk.h>
+#ifdef CONFIG_AXIADO_SGPIO_REGMAP
+#include <linux/regmap.h>
+#endif
+#include "sgpio-axiado.h"
+
+/* Define the global slice array */
+struct ax3000_slice_info slice[MAX_SLICE_COUNT];
+
+static void ax3000_sgpio_set(struct gpio_chip *chip, unsigned int offset, int value)
+{
+	uint32_t bank = GET_BANK(offset);
+	uint32_t position = GET_POS(offset);
+	struct ax3000_sgpio *sgpio = gpiochip_get_data(chip);
+
+	if (value)
+		slice[2].reg_ss[bank] |= 1 << position;
+	else
+		slice[2].reg_ss[bank] &= ~(1 << position);
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_dout_ss + (bank * 4),
+			slice[2].reg_ss[bank]);
+}
+
+static int ax3000_sgpio_get(struct gpio_chip *chip, unsigned int offset)
+{
+	uint32_t bank;
+	uint32_t position;
+	int rc;
+	int direction = 0;
+	struct ax3000_sgpio *sgpio = gpiochip_get_data(chip);
+
+	if (offset >= sgpio->ngpios) {
+		offset -= sgpio->ngpios;
+		direction = 1;
+		offset += (sgpio->max_sgpio_pins - sgpio->ngpios);
+	}
+
+	bank = GET_BANK(offset);
+	position = GET_POS(offset);
+
+	if (direction)
+		rc = IS_BIT_SET(slice[3].reg_ss[bank], position);
+	else
+		rc = IS_BIT_SET(slice[2].reg_ss[bank], position);
+
+	return rc;
+}
+
+static int ax3000_sgpio_dir_in(struct gpio_chip *chip, unsigned int offset)
+{
+	struct ax3000_sgpio *sgpio = gpiochip_get_data(chip);
+
+	if (offset >= sgpio->ngpios)
+		return 0;
+	else
+		return 1;
+}
+
+static int ax3000_sgpio_dir_out(struct gpio_chip *chip, unsigned int offset,
+		int value)
+{
+	struct ax3000_sgpio *sgpio = gpiochip_get_data(chip);
+
+	if (offset < sgpio->ngpios) {
+		if (chip->set)
+			chip->set(chip, offset, value);
+		return 0;
+	} else
+		return 1;
+}
+
+static irqreturn_t sgpio_irq_handler(int irq, void *arg)
+{
+	int i = 0;
+
+	struct ax3000_sgpio *sgpio = (struct ax3000_sgpio *)arg;
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_din_ss,
+			sgpio_reg_read(sgpio, sgpio->regs->slice_status));
+
+	for (i = 0; i < sgpio->max_offset_regs; i++)
+		slice[3].reg_ss[i] = sgpio_reg_read(
+			sgpio, sgpio->regs->slice_din_ss + (i * 4));
+
+	/* Clear Status register */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mask, 0xdfff);
+
+	for (i = 0; i < sgpio->max_sgpio_pins; i++) {
+		if (sgpio->mask_status[i] == 1) {
+			local_irq_disable();
+			generic_handle_irq(sgpio->irq_number[i]);
+			local_irq_enable();
+		}
+	}
+
+	return IRQ_HANDLED;
+}
+
+static void sgpio_hw_init(struct ax3000_sgpio *sgpio)
+{
+	uint32_t bank;
+	uint32_t position;
+	int i = 0;
+
+	/* slice A0, Clock Pin - 0 */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mux_0,
+			0x306); /* sgpio_mux_config */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_preset_0,
+			sgpio->preset_value); /* preset for countl */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_count_0,
+			sgpio->count_value); /* count start value */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_pos_0,
+			0x1f001f); /* pos and pos preset */
+
+	/* Slice B1, Data Load Pin - 1 */
+	bank = GET_BANK((sgpio->ngpios-1));
+	position = GET_POS((sgpio->ngpios-1));
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mux_1,
+			0x304); /* slice_mux_config */
+
+	for (i = 0; i < bank; i++) {
+		sgpio_reg_write(sgpio, sgpio->regs->slice_ld + (i * 4),
+				0xffffffff); /* shift reg */
+		sgpio_reg_write(sgpio, sgpio->regs->slice_ld_ss + (i * 4),
+				0xffffffff); /* shift reg ss */
+	}
+
+	if (position) {
+		sgpio_reg_write(sgpio, sgpio->regs->slice_ld + (i * 4),
+				SET_TILL_POS(slice[1].reg_ss[i],
+					     position)); /* shift reg */
+		sgpio_reg_write(sgpio, sgpio->regs->slice_ld_ss + (i * 4),
+				SET_TILL_POS(slice[1].reg_ss[i],
+					     position)); /* shift reg ss */
+	}
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_preset_1,
+			sgpio->preset_value); /* preset for count */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_count_1,
+			sgpio->count_value); /* count start value */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_pos_1,
+			sgpio->pos_reg); /* pos and pos preset */
+
+	/* Slice C2, Data Out Pin - 2 */
+	bank = GET_BANK((sgpio->ngpios));
+	position = GET_POS((sgpio->ngpios));
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mux_2,
+			0x304); //slice_mux_config
+
+	for (i = 0; i < bank; i++) {
+		sgpio_reg_write(sgpio, sgpio->regs->slice_dout + (i * 4),
+				slice[2].reg_ss[i]); /* shift reg */
+		sgpio_reg_write(sgpio, sgpio->regs->slice_dout_ss + (i * 4),
+				slice[2].reg_ss[i]); /* shift reg ss */
+	}
+
+	if (position) {
+		sgpio_reg_write(sgpio, sgpio->regs->slice_dout + (i * 4),
+				slice[2].reg_ss[i]); /* shift reg */
+		sgpio_reg_write(sgpio, sgpio->regs->slice_dout_ss + (i * 4),
+				slice[2].reg_ss[i]); /* shift reg ss */
+	}
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_preset_2,
+			sgpio->preset_value); /* preset for count */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_count_2,
+			sgpio->count_value); /* count start value */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_pos_2,
+			sgpio->pos_reg); /* pos and pos preset */
+
+	/* Slice D3, Data In Pin - 3 */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mux_3,
+			0x14C); /* slice_mux_config */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_preset_3,
+			sgpio->preset_value); /* preset for count */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_count_3,
+			sgpio->count_value); /* count start value */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_pos_3,
+			sgpio->pos_reg); /* pos and pos preset */
+
+	/* Slice E4, Output Enable for respective pins */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mux_4,
+			0x104); /* slice_mux_config */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_oe,
+			0xffffffff); /* shift reg */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_oe_ss,
+			0xffffffff); /* shift reg ss */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_preset_4,
+			sgpio->preset_value); /* preset for count */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_count_4,
+			sgpio->count_value); /* count start value */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_pos_4,
+			0x1f001f); /* pos and pos preset */
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_mask,
+			0xdfff); /* Enabling the interrupt for new data */
+
+	sgpio_reg_write(sgpio, sgpio->regs->slice_ctrl_en,
+			0xffff); /* cntrl_enable 'b100001111  ckfl d bmgn a */
+	sgpio_reg_write(sgpio, sgpio->regs->slice_ctrl_en_pos,
+			0xffff); /* out_mux_config 'b1000000 */
+}
+
+static int sgpio_set_irq_type(struct irq_data *d, unsigned int type)
+{
+	switch (type) {
+	case IRQ_TYPE_EDGE_BOTH:
+	case IRQ_TYPE_EDGE_RISING:
+	case IRQ_TYPE_EDGE_FALLING:
+		irq_set_handler_locked(d, handle_edge_irq);
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static void sgpio_mask_irq(struct irq_data *d)
+{
+	struct gpio_chip *chip;
+	struct ax3000_sgpio *sgpio;
+	uint32_t irq_num;
+
+	/* Get the gpio_chip associated with the IRQ number */
+	chip = irq_data_get_irq_chip_data(d);  // Get gpio_chip for the IRQ
+	if (!chip) {
+		pr_err("Unable to get gpio_chip for IRQ\n");
+		return;
+	}
+
+	/* Get the private data associated with the gpio_chip */
+	sgpio = gpiochip_get_data(chip);
+	if (!sgpio) {
+		pr_err("Unable to get chip data\n");
+		return;
+	}
+
+	irq_num = irqd_to_hwirq(d);
+	sgpio->irq_number[irq_num] = 0;
+	sgpio->mask_status[irq_num] = 0;
+}
+
+static void sgpio_unmask_irq(struct irq_data *d)
+{
+	struct gpio_chip *chip;
+	struct ax3000_sgpio *sgpio;
+	uint32_t irq_num;
+
+	/* Get the gpio_chip associated with the IRQ number */
+	chip = irq_data_get_irq_chip_data(d);  // Get gpio_chip for the IRQ
+	if (!chip) {
+		pr_err("Unable to get gpio_chip for IRQ\n");
+		return;
+	}
+
+	/* Get the private data associated with the gpio_chip */
+	sgpio = gpiochip_get_data(chip);
+	if (!sgpio) {
+		pr_err("Unable to get chip data\n");
+		return;
+	}
+
+	irq_num = irqd_to_hwirq(d);
+	sgpio->irq_number[irq_num] = gpio_to_irq(sgpio->chip.base + irq_num);
+	sgpio->mask_status[irq_num] = 1;
+}
+
+static int sgpio_probe(struct platform_device *pdev)
+{
+	int rc;
+	int irq;
+	int i;
+	const __be32 *prop;
+	struct gpio_irq_chip *girq;
+	struct ax3000_sgpio *sgpio;
+	uint32_t variant;
+	uint32_t dout_value;
+	int dout_reverse;
+#ifdef CONFIG_AXIADO_SGPIO_REGMAP
+	u32 reg_base_offset = 0;
+#endif
+
+	sgpio = devm_kzalloc(&pdev->dev, sizeof(*sgpio), GFP_KERNEL);
+	if (!sgpio)
+		return -ENOMEM;
+
+#ifdef CONFIG_AXIADO_SGPIO_REGMAP
+	/* Try to get regmap from parent device first */
+	sgpio->regmap = dev_get_regmap(pdev->dev.parent, NULL);
+	if (sgpio->regmap) {
+		/* Get the base address offset from device tree */
+		rc = of_property_read_u32(pdev->dev.of_node, "reg",
+					  &reg_base_offset);
+		if (rc) {
+			dev_err(&pdev->dev, "Failed to read reg property: %d\n",
+				rc);
+			return rc;
+		}
+		sgpio->regmap_base_offset = reg_base_offset;
+		dev_info(&pdev->dev, "Using regmap with base offset: 0x%x\n",
+			 reg_base_offset);
+	} else {
+#endif
+		/* Fall back to direct memory mapping */
+		sgpio->membase = devm_platform_ioremap_resource(pdev, 0);
+		if (IS_ERR(sgpio->membase)) {
+			dev_err(&pdev->dev, "Failed to map SGPIO memory resource\n");
+			return PTR_ERR(sgpio->membase);
+		}
+		dev_info(&pdev->dev, "Using direct memory mapping\n");
+#ifdef CONFIG_AXIADO_SGPIO_REGMAP
+	}
+#endif
+
+	rc = device_property_read_u32(&pdev->dev, "ngpios", &sgpio->ngpios);
+	if (rc < 0) {
+		dev_err(&pdev->dev, "Could not read ngpios property\n");
+		return -EINVAL;
+	}
+
+	if (device_property_read_u32(&pdev->dev, "design-variant", &variant)) {
+		dev_err(&pdev->dev, "design-variant not specified in DT\n");
+		return -EINVAL;
+	}
+
+	/* FPGA B0 SGPIO design variant 128 bit or 512 bit */
+	if (variant == 128) {
+		sgpio->regs = (struct sgpio_reg_offsets *)&design_128_bit_regs;
+		sgpio->max_sgpio_pins = 128;
+		sgpio->max_offset_regs = 4;
+	} else if (variant == 512) {
+		sgpio->regs = (struct sgpio_reg_offsets *)&design_512_bit_regs;
+		sgpio->max_sgpio_pins = 512;
+		sgpio->max_offset_regs = 16;
+	} else {
+		return -EINVAL;
+	}
+
+	if (sgpio->ngpios > sgpio->max_sgpio_pins) {
+		dev_err(&pdev->dev, "ngpio is greater than 512 pins\n");
+		return -EINVAL;
+	}
+
+	rc = device_property_read_u32(&pdev->dev, "bus-frequency", &sgpio->bus_frequency);
+	if (rc < 0) {
+		dev_err(&pdev->dev, "Could not read bus-frequency property\n");
+		return -EINVAL;
+	}
+
+	rc = device_property_read_u32(&pdev->dev, "apb-frequency", &sgpio->apb_frequency);
+	if (rc < 0) {
+		dev_err(&pdev->dev, "Could not read apb-frequency property\n");
+		return -EINVAL;
+	}
+
+	sgpio->preset_value = (sgpio->apb_frequency / sgpio->bus_frequency) - 1;
+	sgpio->count_value = sgpio->preset_value;
+
+	sgpio->pos = sgpio->ngpios - 1;
+	sgpio->pos_reset = sgpio->ngpios - 1;
+
+	sgpio->pos_reg = (sgpio->pos_reset<<16) | sgpio->pos;
+
+	prop = of_get_property(pdev->dev.of_node, "dout-init", NULL);
+	if (!prop) {
+		dev_err(&pdev->dev, "Failed to get dout-init\n");
+		return -EINVAL;
+	}
+
+	for (i = 0; i < sgpio->max_offset_regs; i++) {
+		slice[2].reg_ss[i] = 0;
+		dout_value = be32_to_cpu(prop[i]);
+
+		for (dout_reverse = 0; dout_reverse < 32; ++dout_reverse) {
+			slice[2].reg_ss[i] <<= 1;
+			slice[2].reg_ss[i] |= (dout_value & 1);
+			dout_value >>= 1;
+		}
+	}
+
+	sgpio_hw_init(sgpio);
+
+	irq = irq_of_parse_and_map(pdev->dev.of_node, 0);
+
+	if (irq < 0) {
+		pr_debug("SGPIO : failed to get IRQ = %d\n", irq);
+		return irq;
+	}
+
+	rc = devm_request_threaded_irq(&pdev->dev, irq, NULL,
+			sgpio_irq_handler, IRQF_ONESHOT,
+			"sgpio_irq", sgpio);
+
+	if (rc < 0) {
+		pr_debug("SGPIO: Handler registration failed\n");
+		return rc;
+	}
+
+	sgpio->chip.parent = &pdev->dev;
+	sgpio->chip.ngpio = sgpio->ngpios * 2;
+	sgpio->chip.owner = THIS_MODULE;
+	sgpio->chip.direction_input = ax3000_sgpio_dir_in;
+	sgpio->chip.direction_output = ax3000_sgpio_dir_out;
+	sgpio->chip.get = ax3000_sgpio_get;
+	sgpio->chip.set = ax3000_sgpio_set;
+	sgpio->chip.label = dev_name(&pdev->dev);
+	sgpio->chip.base = -1;
+
+	/* Set up the irqchip dynamically */
+	sgpio->irq.name = "SGPIO-IRQ";
+	sgpio->irq.irq_ack = NULL;
+	sgpio->irq.irq_mask = sgpio_mask_irq;
+	sgpio->irq.irq_unmask = sgpio_unmask_irq;
+	sgpio->irq.irq_set_type = sgpio_set_irq_type;
+	sgpio->irq.flags = IRQCHIP_IMMUTABLE;
+
+	/* Get a pointer to the gpio_irq_chip */
+	girq = &sgpio->chip.irq;
+	girq->chip = &sgpio->irq;
+	/* This will let us handle the parent IRQ in the driver */
+	girq->parent_handler = NULL;
+	girq->num_parents = 0;
+	girq->parents = NULL;
+	girq->default_type = IRQ_TYPE_NONE;
+	girq->handler = handle_bad_irq;
+
+	rc = devm_gpiochip_add_data(&pdev->dev, &sgpio->chip, sgpio);
+	if (rc < 0) {
+		dev_err(&pdev->dev, "Could not register gpiochip, %d\n", rc);
+		return -1;
+	}
+
+	return 0;
+}
+
+static int sgpio_remove(struct platform_device *pdev)
+{
+	return 0;
+}
+
+static const struct of_device_id ax_sgpio_match[] = {
+	{ .compatible = "axiado,sgpio"},
+	{ }
+};
+MODULE_DEVICE_TABLE(of, ax_sgpio_match);
+
+static struct platform_driver sgpio_driver = {
+	.driver = {
+		.name = "sgpio",
+		.owner = THIS_MODULE,
+		.of_match_table = ax_sgpio_match,
+	},
+	.probe = sgpio_probe,
+	.remove = sgpio_remove,
+};
+
+static int __init ax_sgpio_init(void)
+{
+	int ret;
+
+	/* Register the SGPIO Platform driver */
+	ret = platform_driver_register(&sgpio_driver);
+	if (ret < 0) {
+		pr_err("Failed to register SGPIO driver\n");
+		return ret;
+	}
+
+	return 0;
+}
+
+static void __exit ax_sgpio_exit(void)
+{
+	platform_driver_unregister(&sgpio_driver);
+}
+
+module_init(ax_sgpio_init);
+module_exit(ax_sgpio_exit);
+
+MODULE_DESCRIPTION("Axiado Serial GPIO Driver");
+MODULE_AUTHOR("Axiado Corporation");
+MODULE_LICENSE("GPL");
