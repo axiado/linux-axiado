699e77eead264e3119fdc9cb8866e342ec09e593
Tzu-Hao Wei <twei@axiado.com>
2026-04-09 18:29:27 -0700

    phy: axiado: sync Axiado eMMC PHY driver with upstream
    Signed-off-by: Tzu-Hao Wei <twei@axiado.com>

--- drivers/phy/axiado/phy-axiado-emmc.c
+++ drivers/phy/axiado/phy-axiado-emmc.c
@@ -2,14 +2,13 @@
 /*
  * Axiado eMMC PHY driver
  *
- * Copyright (C) 2022-2025 Axiado Corporation (or its affiliates).
+ * Copyright (C) 2017 Arasan Chip Systems Inc.
+ * Copyright (C) 2022-2026 Axiado Corporation (or its affiliates).
  *
  * Based on Arasan Driver (sdhci-pci-arasan.c)
  * sdhci-pci-arasan.c - Driver for Arasan PCI Controller with integrated phy.
- *
- * Copyright (C) 2017 Arasan Chip Systems Inc.
  */
-#include <linux/clk.h>
+#include <linux/bitfield.h>
 #include <linux/delay.h>
 #include <linux/io.h>
 #include <linux/iopoll.h>
@@ -19,10 +18,9 @@
 #include <linux/platform_device.h>
 
 /* Arasan eMMC 5.1 - PHY configuration registers */
-#define CAP_REG_IN_S1_LSB		0x00
 #define CAP_REG_IN_S1_MSB		0x04
 #define PHY_CTRL_1			0x38
-#define PHY_CTRL_2			0x3C
+#define PHY_CTRL_2			0x3c
 #define PHY_CTRL_3			0x40
 #define STATUS				0x50
 
@@ -32,41 +30,25 @@
 #define RETB_ENBL	BIT(1)
 
 #define REN_STRB	BIT(27)
-#define REN_CMD		BIT(12)
-#define REN_DAT0	BIT(13)
-#define REN_DAT1	BIT(14)
-#define REN_DAT2	BIT(15)
-#define REN_DAT3	BIT(16)
-#define REN_DAT4	BIT(17)
-#define REN_DAT5	BIT(18)
-#define REN_DAT6	BIT(19)
-#define REN_DAT7	BIT(20)
-#define REN_CMD_EN	(REN_CMD | REN_DAT0 | REN_DAT1 | REN_DAT2 | \
-		REN_DAT3 | REN_DAT4 | REN_DAT5 | REN_DAT6 | REN_DAT7)
+#define REN_CMD_EN	GENMASK(20, 12)
 
 /* Pull-UP Enable on CMD Line */
-#define PU_CMD		BIT(3)
-#define PU_DAT0		BIT(4)
-#define PU_DAT1		BIT(5)
-#define PU_DAT2		BIT(6)
-#define PU_DAT3		BIT(7)
-#define PU_DAT4		BIT(8)
-#define PU_DAT5		BIT(9)
-#define PU_DAT6		BIT(10)
-#define PU_DAT7		BIT(11)
-#define PU_CMD_EN (PU_CMD | PU_DAT0 | PU_DAT1 | PU_DAT2 | PU_DAT3 | \
-		PU_DAT4 | PU_DAT5 | PU_DAT6 | PU_DAT7)
+#define PU_CMD_EN	GENMASK(11, 3)
 
-/* Slection value for the optimum delay from 1-32 output tap lines */
+/* Selection value for the optimum delay from 1-32 output tap lines */
 #define OTAP_DLY	0x02
 /* DLL charge pump current trim default [1000] */
 #define DLL_TRM_ICP	0x08
 /* Select the frequency range of DLL Operation */
 #define FRQ_SEL	0x01
 
-#define OTAP_SEL(x)		(((x) << 7) | OTAPDLY_EN)
-#define DLL_TRM(x)		(((x) << 22) | DLL_ENBL)
-#define DLL_FRQSEL(x)	((x) << 25)
+#define OTAP_SEL_MASK		GENMASK(10, 7)
+#define DLL_TRM_MASK		GENMASK(25, 22)
+#define DLL_FRQSEL_MASK		GENMASK(27, 25)
+
+#define OTAP_SEL(x)		(FIELD_PREP(OTAP_SEL_MASK, x) | OTAPDLY_EN)
+#define DLL_TRM(x)		(FIELD_PREP(DLL_TRM_MASK, x) | DLL_ENBL)
+#define DLL_FRQSEL(x)	FIELD_PREP(DLL_FRQSEL_MASK, x)
 
 #define OTAPDLY_EN	BIT(11)
 
@@ -79,72 +61,51 @@
 #define MAX_CLK_BUF1	BIT(21)
 #define MAX_CLK_BUF2	BIT(22)
 
-#define CLK_MULTIPLIER	0xC008E
-#define LOOP_TIMEOUT	3000
-#define TIMEOUT_DELAY	100
+#define CLK_MULTIPLIER	0xc008e
+#define POLL_TIMEOUT_MS	3000
+#define POLL_DELAY_US	100
 
 struct axiado_emmc_phy {
-	void __iomem    *reg_base;
+	void __iomem *reg_base;
+	struct device *dev;
 };
-
-static void arasan_emmc_phy_write(struct axiado_emmc_phy *ax_phy, u32 offset, u32 data)
-{
-	writel(data, ax_phy->reg_base + offset);
-}
-
-static int arasan_emmc_phy_read(struct axiado_emmc_phy *ax_phy, u32 offset)
-{
-	u32 val = readl(ax_phy->reg_base + offset);
-
-	return val;
-}
 
 static int axiado_emmc_phy_init(struct phy *phy)
 {
-	u32 val;
-	ktime_t timeout;
-
 	struct axiado_emmc_phy *ax_phy = phy_get_drvdata(phy);
+	struct device *dev = ax_phy->dev;
+	u32 val;
+	int ret;
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_1);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_1, val | RETB_ENBL | RTRIM_EN);
+	val = readl(ax_phy->reg_base + PHY_CTRL_1);
+	writel(val | RETB_ENBL | RTRIM_EN, ax_phy->reg_base + PHY_CTRL_1);
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_3);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_3, val | PDB_ENBL);
+	val = readl(ax_phy->reg_base + PHY_CTRL_3);
+	writel(val | PDB_ENBL, ax_phy->reg_base + PHY_CTRL_3);
 
-	/* Wait max 3000 ms */
-	timeout = ktime_add_ms(ktime_get(), LOOP_TIMEOUT);
-
-	while (1) {
-		bool timedout = ktime_after(ktime_get(), timeout);
-
-		if (arasan_emmc_phy_read(ax_phy, STATUS) & CALDONE_MASK)
-			break;
-
-		if (timedout) {
-			dev_err(&phy->dev, "CALDONE_MASK bit is not cleared.");
-			return -ETIMEDOUT;
-		}
-		udelay(TIMEOUT_DELAY);
+	ret = readl_poll_timeout(ax_phy->reg_base + STATUS, val,
+				 val & CALDONE_MASK, POLL_DELAY_US,
+				 POLL_TIMEOUT_MS * 1000);
+	if (ret) {
+		dev_err(dev, "PHY calibration timeout\n");
+		return ret;
 	}
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_1);
+	val = readl(ax_phy->reg_base + PHY_CTRL_1);
+	writel(val | REN_CMD_EN | PU_CMD_EN, ax_phy->reg_base + PHY_CTRL_1);
 
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_1, val | REN_CMD_EN | PU_CMD_EN);
+	val = readl(ax_phy->reg_base + PHY_CTRL_2);
+	writel(val | REN_STRB, ax_phy->reg_base + PHY_CTRL_2);
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_2);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_2, val | REN_STRB);
+	val = readl(ax_phy->reg_base + PHY_CTRL_3);
+	writel(val | MAX_CLK_BUF0 | MAX_CLK_BUF1 | MAX_CLK_BUF2,
+	       ax_phy->reg_base + PHY_CTRL_3);
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_3);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_3, val | MAX_CLK_BUF0 |
-			MAX_CLK_BUF1 | MAX_CLK_BUF2);
+	writel(CLK_MULTIPLIER, ax_phy->reg_base + CAP_REG_IN_S1_MSB);
 
-	val = arasan_emmc_phy_read(ax_phy, CAP_REG_IN_S1_MSB);
-	arasan_emmc_phy_write(ax_phy, CAP_REG_IN_S1_MSB, CLK_MULTIPLIER);
-
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_3);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_3, val | SEL_DLY_RXCLK |
-			SEL_DLY_TXCLK);
+	val = readl(ax_phy->reg_base + PHY_CTRL_3);
+	writel(val | SEL_DLY_RXCLK | SEL_DLY_TXCLK,
+	       ax_phy->reg_base + PHY_CTRL_3);
 
 	return 0;
 }
@@ -152,56 +113,62 @@ static int axiado_emmc_phy_init(struct phy *phy)
 static int axiado_emmc_phy_power_on(struct phy *phy)
 {
 	struct axiado_emmc_phy *ax_phy = phy_get_drvdata(phy);
-
+	struct device *dev = ax_phy->dev;
 	u32 val;
-	ktime_t timeout;
+	int ret;
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_1);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_1, val | RETB_ENBL);
+	val = readl(ax_phy->reg_base + PHY_CTRL_1);
+	writel(val | RETB_ENBL, ax_phy->reg_base + PHY_CTRL_1);
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_3);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_3, val | PDB_ENBL);
+	val = readl(ax_phy->reg_base + PHY_CTRL_3);
+	writel(val | PDB_ENBL, ax_phy->reg_base + PHY_CTRL_3);
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_2);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_2, val | OTAP_SEL(OTAP_DLY));
+	val = readl(ax_phy->reg_base + PHY_CTRL_2);
+	writel(val | OTAP_SEL(OTAP_DLY), ax_phy->reg_base + PHY_CTRL_2);
 
-	arasan_emmc_phy_read(ax_phy, PHY_CTRL_2);
+	val = readl(ax_phy->reg_base + PHY_CTRL_1);
+	writel(val | DLL_TRM(DLL_TRM_ICP), ax_phy->reg_base + PHY_CTRL_1);
 
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_1);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_1, val | DLL_TRM(DLL_TRM_ICP));
+	val = readl(ax_phy->reg_base + PHY_CTRL_3);
+	writel(val | DLL_FRQSEL(FRQ_SEL), ax_phy->reg_base + PHY_CTRL_3);
 
-	arasan_emmc_phy_write(ax_phy, STATUS, 0x00);
-
-	val = arasan_emmc_phy_read(ax_phy, PHY_CTRL_3);
-	arasan_emmc_phy_write(ax_phy, PHY_CTRL_3, val | DLL_FRQSEL(FRQ_SEL));
-
-	/* Wait max 3000 ms */
-	timeout = ktime_add_ms(ktime_get(), LOOP_TIMEOUT);
-
-	while (1) {
-		bool timedout = ktime_after(ktime_get(), timeout);
-
-		if (arasan_emmc_phy_read(ax_phy, STATUS) & DLL_RDY_MASK)
-			break;
-
-		if (timedout) {
-			dev_err(&phy->dev, "DLL_RDY_MASK bit is not cleared.");
-			return -ETIMEDOUT;
-		}
-		udelay(TIMEOUT_DELAY);
+	ret = read_poll_timeout(readl, val, val & DLL_RDY_MASK, POLL_DELAY_US,
+				POLL_TIMEOUT_MS * 1000, false,
+				ax_phy->reg_base + STATUS);
+	if (ret) {
+		dev_err(dev, "DLL ready timeout\n");
+		return ret;
 	}
+
+	return 0;
+}
+
+static int axiado_emmc_phy_power_off(struct phy *phy)
+{
+	struct axiado_emmc_phy *ax_phy = phy_get_drvdata(phy);
+	u32 val;
+
+	val = readl(ax_phy->reg_base + PHY_CTRL_1);
+	val &= ~(DLL_TRM_MASK | DLL_ENBL);
+	writel(val, ax_phy->reg_base + PHY_CTRL_1);
+
+	val = readl(ax_phy->reg_base + PHY_CTRL_3);
+	val &= ~(DLL_FRQSEL_MASK | PDB_ENBL);
+	writel(val, ax_phy->reg_base + PHY_CTRL_3);
+
 	return 0;
 }
 
 static const struct phy_ops axiado_emmc_phy_ops = {
-	.init		= axiado_emmc_phy_init,
-	.power_on	= axiado_emmc_phy_power_on,
-	.owner		= THIS_MODULE,
+	.init = axiado_emmc_phy_init,
+	.power_on = axiado_emmc_phy_power_on,
+	.power_off = axiado_emmc_phy_power_off,
+	.owner = THIS_MODULE,
 };
 
 static const struct of_device_id axiado_emmc_phy_of_match[] = {
-	{ .compatible = "axiado,ax3000-emmc-phy"},
-	{}
+	{ .compatible = "axiado,ax3000-emmc-phy" },
+	{ /* sentinel */ },
 };
 MODULE_DEVICE_TABLE(of, axiado_emmc_phy_of_match);
 
@@ -210,9 +177,7 @@ static int axiado_emmc_phy_probe(struct platform_device *pdev)
 	struct axiado_emmc_phy *ax_phy;
 	struct phy_provider *phy_provider;
 	struct device *dev = &pdev->dev;
-	const struct of_device_id *id;
 	struct phy *generic_phy;
-	struct resource *res;
 
 	if (!dev->of_node)
 		return -ENODEV;
@@ -221,24 +186,16 @@ static int axiado_emmc_phy_probe(struct platform_device *pdev)
 	if (!ax_phy)
 		return -ENOMEM;
 
-	res = platform_get_resource(pdev, IORESOURCE_MEM, 0);
-
-	ax_phy->reg_base = devm_ioremap_resource(&pdev->dev, res);
-
+	ax_phy->reg_base = devm_platform_ioremap_resource(pdev, 0);
 	if (IS_ERR(ax_phy->reg_base))
 		return PTR_ERR(ax_phy->reg_base);
 
-	id = of_match_node(axiado_emmc_phy_of_match, pdev->dev.of_node);
-	if (!id) {
-		dev_err(dev, "failed to get match_node\n");
-		return -EINVAL;
-	}
+	ax_phy->dev = dev;
 
 	generic_phy = devm_phy_create(dev, dev->of_node, &axiado_emmc_phy_ops);
-	if (IS_ERR(generic_phy)) {
-		dev_err(dev, "failed to create PHY\n");
-		return PTR_ERR(generic_phy);
-	}
+	if (IS_ERR(generic_phy))
+		return dev_err_probe(dev, PTR_ERR(generic_phy),
+				     "failed to create PHY\n");
 
 	phy_set_drvdata(generic_phy, ax_phy);
 	phy_provider = devm_of_phy_provider_register(dev, of_phy_simple_xlate);
@@ -247,9 +204,9 @@ static int axiado_emmc_phy_probe(struct platform_device *pdev)
 }
 
 static struct platform_driver axiado_emmc_phy_driver = {
-	.probe		 = axiado_emmc_phy_probe,
-	.driver		 = {
-		.name	 = "axiado-emmc-phy",
+	.probe = axiado_emmc_phy_probe,
+	.driver = {
+		.name = "axiado-emmc-phy",
 		.of_match_table = axiado_emmc_phy_of_match,
 	},
 };
